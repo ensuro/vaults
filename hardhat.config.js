e8b39fb6b8ccd1cfd742107aa0bee1c88f8e1e7d
ndelatorre <ndelatorre@itba.edu.ar>
2023-11-22 16:30:40 -0300

    more repo config

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+require("hardhat-dependency-compiler");
+require("hardhat-contract-sizer");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.16",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: false,
+    disambiguatePaths: false,
+  },
+};
