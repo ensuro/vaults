5b35795c6aec873d5def2e8d6c8d3f51d731c966
ndelatorre <ndelatorre@itba.edu.ar>
2023-12-04 10:06:55 -0300

    collector and withdrawer mocks

---
+++ contracts/mock/CollectorMock.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+import {ICallable} from "../interfaces/ICallable.sol";
+import {SmartVaultMock} from "./SmartVaultMock.sol";
+
+contract CollectorMock is ICallable {
+  address internal _smartVault;
+
+  constructor(address smartVault_) {
+    _smartVault = smartVault_;
+  }
+
+  function call(address token, uint256 amount) external view {
+    SmartVaultMock(_smartVault).deposit(msg.sender, token, amount);
+    return;
+  }
+}
