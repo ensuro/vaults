15968c8b60b148510e5203d16bce1c1f168601f5
LucasCambon <lucambon08@gmail.com>
2024-12-11 10:47:42 -0300

    Fix timeout error

--- test/hardhat-retry.js
+++ test/hardhat-retry.js
@@ -18,7 +18,7 @@ class BackoffRetry extends ProviderWrapper {
         return await this._wrappedProvider.request(args);
       } catch (e) {
         if (!(e instanceof ProviderError) || i >= MAX_RETRIES - 1) throw e;
-        if (e.code === -32000 && e.message.includes("header not found")) {
+        if (e.code === -32000 && (e.message.includes("header not found") || e.message.includes("timeout"))) {
           console.error("Retrying %s because of temp error %s: %s (%s)", args.method, e.code, e.message, e.data);
           await delay(BACKOFF_DELAY_MS);
           continue;
