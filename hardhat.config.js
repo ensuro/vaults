052501cbc7415c4b66e7d51beeb0cf9f3d8d5227
LucasCambon <lucambon08@gmail.com>
2026-01-22 15:17:48 -0300

    Deleted TestCurrencyAC

--- hardhat.config.js
+++ hardhat.config.js
@@ -38,7 +38,6 @@ module.exports = {
   dependencyCompiler: {
     paths: [
       "@ensuro/utils/contracts/TestCurrency.sol",
-      "@ensuro/utils/contracts/TestCurrencyAC.sol",
       "@ensuro/utils/contracts/TestERC4626.sol",
       "@ensuro/swaplibrary/contracts/mocks/SwapRouterMock.sol",
       "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol",
